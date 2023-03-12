7e87aeb4e6c86d8640ff3d4a1ee8a8450873147b
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-12 21:13:44 +0800

    refactor: trigger simplify, simplified really a lot

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doozy (多嘴)
 // @namespace    https://github.com/mefengl
-// @version      0.3.5
+// @version      0.3.6
 // @description  A wonderful day spent with ChatGPT
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
 // @author       mefengl
@@ -71,133 +71,88 @@
   }
   update_menu();
 
-  // douban_book
   const douban_book_prompts = [
-    (title, author) => `${author}的《${title}》的主要观点列成表格会是：`,
-    (title, author) => `${author}的《${title}》比较重要的章节会是：`,
-    (title, author) => `${author}的《${title}》在哪些方面是有争议的会是：`,
-    (title, author) => `${author}的《${title}》当人们生活在不同的时代时，会有什么不同的观点会是：`,
-    (title, author) => `${author}的《${title}》综合Goodreads评分和豆瓣等各种评分和评价会是：`,
-    (title, author) => `${author}的《${title}》的类似书籍或文章和它们的区别会是：`,
-    (title, author) => `${author}的《${title}》的观点相反的书籍或文章和对应的观点会是：`,
+    ({ title, author }) => `${author}的《${title}》的主要观点列成表格会是：`,
+    ({ title, author }) => `${author}的《${title}》比较重要的章节会是：`,
+    ({ title, author }) => `${author}的《${title}》在哪些方面是有争议的会是：`,
+    ({ title, author }) => `${author}的《${title}》当人们生活在不同的时代时，会有什么不同的观点会是：`,
+    ({ title, author }) => `${author}的《${title}》综合Goodreads评分和豆瓣等各种评分和评价会是：`,
+    ({ title, author }) => `${author}的《${title}》的类似书籍或文章和它们的区别会是：`,
+    ({ title, author }) => `${author}的《${title}》的观点相反的书籍或文章和对应的观点会是：`,
   ]
-  // a cache for lastest 10 title-author
-  menu_all.douban_book && $(() => {
-    if (location.href.includes("book.douban.com/subject")) {
-      const title = $("meta[property='og:title']").attr("content");
-      const author = $("meta[property='book:author']").attr("content");
-      const douban_book_cache = GM_getValue("douban_book_cache", []);
-      // if not in cache, add it, or return
-      // if (!douban_book_cache.some(item => item.title == title && item.author == author)) {
-      if (true) {
-        douban_book_cache.push({ title, author });
-        if (douban_book_cache.length > 10) {
-          douban_book_cache.shift();
-        }
-        GM_setValue("douban_book_cache", douban_book_cache);
-
-        // trigger ChatGPT
-        const prompt_texts = douban_book_prompts.map(prompt => prompt(title, author));
-        GM_setValue("prompt_texts", prompt_texts);
-      } else { return; }
-    }
-  });
-
-  // question
   const question_prompts = [
-    (question) => `问题：${question}，暗含的观点是：`,
-    (question) => `问题：${question}，应该去反思：`,
-    (question) => `问题：${question}，想要改进或解决它，可以从这些方面入手：`,
-    (question) => `问题：${question}，提问者和提问者的目的是：`,
-    (question) => `问题：${question}，问题的相关历史是：`,
-    (question) => `问题：${question}，不同的国家对这个问题的看法会是：`,
-    (question) => `问题：${question}，类似问题和它们的区别会是：`,
-    (question) => `问题：${question}，观点相反的问题和对应的观点会是：`,
-    (question) => `问题：${question}，幽默的回答会是：`,
-    (question) => `问题：${question}，主要观点列成表格会是：`,
-    (question) => `问题：${question}，相关书籍、文章、视频或网站会是：`,
-    (question) => `从这个问题：${question}，可以引申出这些问题：`,
+    ({ question }) => `问题：${question}，暗含的观点是：`,
+    ({ question }) => `问题：${question}，应该去反思：`,
+    ({ question }) => `问题：${question}，想要改进或解决它，可以从这些方面入手：`,
+    ({ question }) => `问题：${question}，提问者和提问者的目的是：`,
+    ({ question }) => `问题：${question}，问题的相关历史是：`,
+    ({ question }) => `问题：${question}，不同的国家对这个问题的看法会是：`,
+    ({ question }) => `问题：${question}，类似问题和它们的区别会是：`,
+    ({ question }) => `问题：${question}，观点相反的问题和对应的观点会是：`,
+    ({ question }) => `问题：${question}，幽默的回答会是：`,
+    ({ question }) => `问题：${question}，主要观点列成表格会是：`,
+    ({ question }) => `问题：${question}，相关书籍、文章、视频或网站会是：`,
+    ({ question }) => `从这个问题：${question}，可以引申出这些问题：`,
   ]
-
-  // // zhihu
   const zhihu_prompts = [...question_prompts];
-
-  menu_all.zhihu && $(() => {
-    if (location.href.includes("zhihu.com/question")) {
-      const question = $('meta[itemprop="name"]').attr('content');
-      const zhihu_cache = GM_getValue("zhihu_cache", []);
-      // if not in cache, add it, or return
-      // if (!zhihu_cache.some(item => item.question == question)) {
-      if (true) {
-        zhihu_cache.push({ question });
-        if (zhihu_cache.length > 10) {
-          zhihu_cache.shift();
-        }
-        GM_setValue("zhihu_cache", zhihu_cache);
-
-        // trigger ChatGPT
-        const prompt_texts = zhihu_prompts.map(prompt => prompt(question));
-        GM_setValue("prompt_texts", prompt_texts);
-      } else { return; }
-    }
-  });
-
-  // // hackernews
-
   const hackernews_prompts = [...question_prompts];
-
-  menu_all.hackernews && $(() => {
-    if (location.href.includes("news.ycombinator.com/item")) {
-      const question = $('td.title > span.titleline > a').text();
-      const hackernews_cache = GM_getValue("hackernews_cache", []);
-      // if not in cache, add it, or return
-      // if (!hackernews_cache.some(item => item.question == question)) {
-      if (true) {
-        hackernews_cache.push({ question });
-        if (hackernews_cache.length > 10) {
-          hackernews_cache.shift();
-        }
-        GM_setValue("hackernews_cache", hackernews_cache);
-
-        // trigger ChatGPT
-        const prompt_texts = hackernews_prompts.map(prompt => prompt(question));
-        GM_setValue("prompt_texts", prompt_texts);
-      } else { return; }
-    }
-  });
-
-  // github
   const github_prompts = [
-    (website) => `${website}的主要功能列成表格是：`,
-    (website) => `${website}的最佳实践是：`,
-    (website) => `${website}的类似项目是：`,
-    (website) => `${website}的相关书籍、文章、视频或网站是：`,
-    (website) => `${website}的下一代项目是：`,
-    (website) => `${website}的应用场景是：`,
+    ({ website }) => `${website}的主要功能列成表格是：`,
+    ({ website }) => `${website}的最佳实践是：`,
+    ({ website }) => `${website}的类似项目是：`,
+    ({ website }) => `${website}的相关书籍、文章、视频或网站是：`,
+    ({ website }) => `${website}的下一代项目是：`,
+    ({ website }) => `${website}的应用场景是：`,
   ]
 
-  menu_all.github && $(() => {
-    // github repo?
-    if (!location.href.includes("github.com")) { return; }
-    const parts = location.href.split("/");
-    if (parts.length >= 5 && parts[parts.length - 2] && parts[parts.length - 1]) {
-      const github_cache = GM_getValue("github_cache", []);
-      // if (!github_cache.some(item => item.website == location.href)) {
-      if (true) {
-        github_cache.push({ website: location.href });
-        if (github_cache.length > 10) {
-          github_cache.shift();
+  function chatgpt_trigger(prompt_prepare, prompts) {
+    const prepare_data = prompt_prepare();
+    const prompt_texts = prompts.map(prompt => prompt(prepare_data));
+    GM_setValue("prompt_texts", prompt_texts);
+  }
+
+  const triggers = [
+    {
+      checker: () => menu_all.douban_book && location.href.includes("book.douban.com/subject"),
+      prepare: () => {
+        const title = $("meta[property='og:title']").attr("content");
+        const author = $("meta[property='book:author']").attr("content");
+        return { title, author };
+      },
+      prompts: douban_book_prompts
+    },
+    {
+      checker: () => menu_all.zhihu && location.href.includes("zhihu.com/question"),
+      prepare: () => {
+        const question = $('meta[itemprop="name"]').attr('content');
+        return { question };
+      },
+      prompts: zhihu_prompts
+    },
+    {
+      checker: () => menu_all.hackernews && location.href.includes("news.ycombinator.com/item"),
+      prepare: () => {
+        const question = $('td.title > span.titleline > a').text();
+        return { question };
+      },
+      prompts: hackernews_prompts
+    },
+    {
+      checker: () => menu_all.github && location.href.includes("github.com"),
+      prepare: () => {
+        const parts = location.href.split("/");
+        if (parts.length >= 5 && parts[parts.length - 2] && parts[parts.length - 1]) {
+          const website = parts[parts.length - 2] + "/" + parts[parts.length - 1];
+          return { website };
         }
-        GM_setValue("github_cache", github_cache);
+      },
+      prompts: github_prompts
+    },
+  ];
 
-        // trigger ChatGPT
-        const prompt_texts = github_prompts.map(prompt => prompt(location.href));
-        GM_setValue("prompt_texts", prompt_texts);
-      } else { return; }
-    }
+  triggers.forEach(trigger => {
+    trigger.checker() && chatgpt_trigger(trigger.prepare, trigger.prompts);
   });
-
-
 
   /* ************************************************************************* */
   // ChatGPT response to prompt_texts
